f761918e72a67087b3923b60b5210969a4df802a
Your Name <jpsaltz@gmail.com>
2017-10-03 14:36:59 -0700

    Added components folder under src/ contains a test component for infoPane that currently isn't working

---
+++ github-comparator/src/components/infoPane.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+class infoPane extends Component {
+  render() {
+    return (
+      <div>
+        <p>test component</p>
+      </div>
+    );
+  }
+}
+
+export default infoPane;
