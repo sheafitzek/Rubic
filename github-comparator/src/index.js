f1123a90b99898e923e72a2cafd8fa1f3889de59
Poitier Stringer <stringerpoitier@gmail.com>
2017-10-08 13:38:40 -0700

    made getPlayers method available as a prop in Player Select

--- github-comparator/src/index.js
+++ github-comparator/src/index.js
@@ -15,12 +15,18 @@ const Root = ()=> {
 		this.props.player1 = player1;
 		this.props.player2 = player2;
 	}
-
+	const SelectPlayers = (props) => {
+		return (
+			<PlayerSelect
+				getPlayers = {getPlayers}
+			/>
+		);
+	}
 	return(
 		<BrowserRouter>
 			<div>
 				<Switch>
-					<Route exact path="/" component={PlayerSelect} getPlayers={this.getPlayers} />
+					<Route exact path="/" component={SelectPlayers}/>
 					{/*<Route path="/compare/" component={App}/>*/}
 					<Route path="/compare/" render={(props)=> ( <App player1={this.player1} player2={this.player2}/> )} />
 					<Route component={NotFound}/>
