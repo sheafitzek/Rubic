e2ff9bd29569b4215642b3dc7a0662f2996d1687
sheafitzek <agg817@sbcglobal.net>
2017-10-12 22:42:37 -0500

    clean up & comment Index.js, PlayerSelect.js

--- github-comparator/src/components/PlayerSelect/PlayerSelect.js
+++ github-comparator/src/components/PlayerSelect/PlayerSelect.js
@@ -5,32 +5,44 @@ import './PlayerSelect.css';
 import PropTypes from 'prop-types';
 
 class PlayerSelect extends React.Component {
-
 	goToApp(e) {
 		e.preventDefault();
 
-		const players = `${this.Player1.value}-vs-${this.player2.value}`;
+		// variable to include in route below
+		const players = `${this.player1.value}-vs-${this.player2.value}`;
 
-		// callback from Index.js
+		// invoke callback function passed as props from Index.js
 		this.props.getPlayers(`${this.player1.value}`, `${this.player2.value}`);
 
+		// compose a route with the above variable that should lead to App.js
 		this.context.router.transitionTo(`/compare/${players}`);
 	}
 
 	render() {
 		return (
 			<form className="player-selector" onSubmit={(e)=> this.goToApp(e)}> {/* inline binding */}
+				{/* input value from forms should update player1 & player2 props in the getPlayers function */}
 				<h2>Please Enter the Competitors</h2>
 				<p>You must either enter two github usernames or two github repos</p>
-				<input type="text" placeholder="User/Repo Name" ref={(input)=> {this.Player1 = input}} required/>
-				<input type="text" placeholder="User/Repo Name" ref={(input)=> {this.Player2 = input}} required/>
+				<input
+					type="text"
+					placeholder="User/Repo Name"
+					ref={(input)=> {this.player1 = input}}
+					required
+				/>
+				<input
+					type="text"
+					placeholder="User/Repo Name"
+					ref={(input)=> {this.player2 = input}}
+					required
+				/>
 				<button type="submit">Compare 🡒</button>
 			</form>
 		)
 	}
 }
 
-// access BrowserRouter from index.js
+// needed to access BrowserRouter from index.js & compose the route above
 PlayerSelect.contextTypes = {
 	router: PropTypes.object,
 }
