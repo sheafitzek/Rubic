92cbd5d4ff9aea67aaec6ed45f54351ddbe139b8
sheafitzek <agg817@sbcglobal.net>
2017-10-26 17:57:59 -0500

    parsed all relevant data from api calls

--- github-comparator/src/js/api.js
+++ github-comparator/src/js/api.js
@@ -1,17 +1,31 @@
-// url for basic user stats: https://api.github.com/users/<user>
-// url for user commit stats: https://api.github.com/search/commits?q=committer:<user>
-// url for user repo stats: https://api.github.com/search/repositories?q=user:<user>
-
 import axios from 'axios';
+import moment from 'moment';
 
 export function apiQuery(player) {
-	// console.log(`1: this`, this);
-
 	this.getPlayerStats(player).then((data) => {
-		// console.log(`3: this`, this);
-		// console.log(`4: this.state`, this.state);
-		// console.log(`5: data`, data);
-		const languages = data.repoData.data.items.reduce((sum, item, index) => {
+		let userData = data.userData.data;
+		let commitData = data.commitData.data;
+		let repoData = data.roughRepoData.reduce((a, b) => a.concat(b), []);
+
+		const stargazers = repoData.reduce((sum, item, index) => {
+			sum += item.stargazers_count;
+
+			return sum;
+		}, 0);
+
+		const forks = repoData.reduce((sum, item, index) => {
+			item.fork === true && sum++;
+
+			return sum;
+		}, 0);
+
+		const hasBeenForked = repoData.reduce((sum, item, index) => {
+			sum += item.stargazers_count;
+
+			return sum;
+		}, 0);
+
+		const languages = repoData.reduce((sum, item, index) => {
 			item.language in sum ? sum[item.language]++ : (sum[item.language] = 1);
 
 			return sum;
@@ -21,19 +35,47 @@ export function apiQuery(player) {
 			return languages[a] > languages[b] ? a : b;
 		});
 
+		const formatDate = function(date, format) {
+			let diff = moment(date).diff(moment(), 'milliseconds');
+			let duration = moment.duration(diff);
+
+			let since = moment(date).format('MM/DD/YYYY');
+			let compare = `${-duration.asYears().toFixed(2)} years`;
+			let display = `${-duration.years()} yrs, ${-duration.months()} mos, ${-duration.days()} days, ${-duration.hours()} hrs`;
+
+			return format === `since` ? since : format === `compare` ? compare : display;
+		};
+
+		const memberSince = formatDate(userData.created_at, `since`);
+		const memberTimeCompare = formatDate(userData.created_at, `compare`);
+		const memberTimeDisplay = formatDate(userData.created_at, `display`);
+
 		if (player === this.props.player1) {
 			this.setState({
 				users : Object.assign(this.state.users, {
 					player1 : {
-						avatar    : data.userData.data.avatar_url,
-						handle    : data.userData.data.login,
-						name      : data.userData.data.name,
-						type      : data.userData.data.type,
-						repos     : data.userData.data.public_repos,
-						gists     : data.userData.data.public_gists,
-						followers : data.userData.data.followers,
-						following : data.userData.data.following,
-						language  : topLanguage,
+						// profile data
+						avatar            : userData.avatar_url,
+						handle            : userData.login,
+						name              : userData.name,
+						type              : userData.type,
+						location          : userData.html_url,
+						memberSince       : memberSince,
+
+						// comparison data
+						pubReposOwned     : data.pubReposOwned,
+						pubReposContrib   : userData.public_repos,
+						commits           : commitData.total_count,
+						gists             : userData.public_gists,
+						followers         : userData.followers,
+						following         : userData.following,
+						stargazers        : stargazers,
+						forks             : forks,
+						hasBeenForked     : hasBeenForked,
+						languages         : languages,
+						topLanguage       : topLanguage,
+						memberTimeCompare : memberTimeCompare,
+						memberTimeDisplay : memberTimeDisplay,
 					},
 				}),
 			});
@@ -41,27 +83,39 @@ export function apiQuery(player) {
 			this.setState({
 				users : Object.assign(this.state.users, {
 					player2 : {
-						avatar    : data.userData.data.avatar_url,
-						handle    : data.userData.data.login,
-						name      : data.userData.data.name,
-						type      : data.userData.data.type,
-						repos     : data.userData.data.public_repos,
-						gists     : data.userData.data.public_gists,
-						followers : data.userData.data.followers,
-						following : data.userData.data.following,
-						language  : topLanguage,
+						// profile data
+						avatar            : userData.avatar_url,
+						handle            : userData.login,
+						name              : userData.name,
+						type              : userData.type,
+						location          : userData.html_url,
+						memberSince       : memberSince,
+
+						// comparison data
+						pubReposOwned     : data.pubReposOwned,
+						pubReposContrib   : userData.public_repos,
+						commits           : commitData.total_count,
+						gists             : userData.public_gists,
+						followers         : userData.followers,
+						following         : userData.following,
+						stargazers        : stargazers,
+						forks             : forks,
+						hasBeenForked     : hasBeenForked,
+						languages         : languages,
+						topLanguage       : topLanguage,
+						memberTimeCompare : memberTimeCompare,
+						memberTimeDisplay : memberTimeDisplay,
 					},
 				}),
 			});
 		}
-		// console.log(`6: this.state`, this.state);
 	});
 }
 
 export function getPlayerStats(player) {
 	const userUrl = `https://api.github.com/users/${player}`;
-	const repoUrl = `https://api.github.com/search/repositories?q=user:${player}`;
-	const commitUrl = `https://api.github.com/search/commits?q=committer:${player}`;
+	const repoUrl = `https://api.github.com/search/repositories?q=user:${player}&per_page=100`;
+	const commitUrl = `https://api.github.com/search/commits?q=committer:${player}&per_page=100`;
 
 	const commitUrlConfig = {
 		headers : {
@@ -69,17 +123,58 @@ export function getPlayerStats(player) {
 		},
 	};
 
-	return axios
-		.all([axios.get(userUrl), axios.get(repoUrl), axios.get(commitUrl, commitUrlConfig)])
-		.then(
-			axios.spread(function(userData, repoData, commitData) {
-				// console.log(`2: data`, userData.data);
-				// console.log(`2: data`, repoData.data);
-				// console.log(`2: data`, commitData.data);
-				return {userData, repoData, commitData};
+	let data = {};
+	let roughRepoData = [];
+	let pubReposOwned;
+
+	function getRepoStats(link) {
+		let linkHeaders;
+		const LastLinkMatch = new RegExp(/([0-9]*)>; rel="last"/, ``);
+		let lastLink;
+		let promises = [fetch(link)];
+
+		return fetch(link)
+			.then((response) => {
+				linkHeaders = response.headers.get('Link');
+				if (linkHeaders !== null && linkHeaders.includes(`rel="next"`)) {
+					lastLink = linkHeaders.match(LastLinkMatch)[1];
+
+					for (let i = 2; i <= lastLink; i++) {
+						promises.push(
+							fetch(
+								`https://api.github.com/search/repositories?q=user:${player}&per_page=100&page=${i}`
+							)
+						);
+					}
+				}
 			})
-		)
-		.catch(function(error) {
-			console.log(error);
-		});
+			.then(() => {
+				return Promise.all(promises);
+			})
+			.then((data) => {
+				data.forEach((item) => {
+					item.json().then((data) => {
+						roughRepoData.push(data.items);
+						pubReposOwned = data.total_count;
+					});
+				});
+			});
+	}
+
+	return getRepoStats(repoUrl).then(() => {
+		return axios
+			.all([axios.get(userUrl), axios.get(commitUrl, commitUrlConfig)])
+			.then(
+				axios.spread(function(userData, commitData) {
+					return {userData, commitData};
+				})
+			)
+			.then((userData, commitData) => {
+				Object.assign(data, userData, commitData, {roughRepoData}, {pubReposOwned});
+				return data;
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	});
 }
